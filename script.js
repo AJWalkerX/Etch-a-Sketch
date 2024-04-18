0ad9daa00330afcca4de346953ce3b28278076ad
AJWalkerX <87567671+AJWalkerX@users.noreply.github.com>
2024-04-18 10:12:03 +0300

    createSize function

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    createSize(16)
+})
 
+function createSize(size){
+    const container = document.querySelector(".container");
 
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
+    let numDivs = size * size;
+
+    for(let i = 0; i < numDivs; i++){
+        let div = document.createElement("div");
+        container.insertAdjacentElement("beforeend", div);
+    }
+}
