fa9e14582a6e9672dcf12274090a65bda1d55eae
AJWalkerX <87567671+AJWalkerX@users.noreply.github.com>
2024-04-18 12:33:35 +0300

    working with the buttons

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+let color = "black";
+
 document.addEventListener("DOMContentLoaded", ()=>{
-    createSize(16)
+    createSize(16);
+    const btn_changeSize = document.getElementById("changeGrid");
+    btn_changeSize.addEventListener("click", ()=>{
+        let size = getSize();
+        createSize(size);
+    });
 })
 
 function createSize(size){
@@ -12,6 +19,48 @@ function createSize(size){
 
     for(let i = 0; i < numDivs; i++){
         let div = document.createElement("div");
+        div.addEventListener("mouseover", colorDiv);
         container.insertAdjacentElement("beforeend", div);
     }
+    let grid = container.children;
+    if(grid.length > 0){
+        for(let i = 0; i < grid.length; i++){
+            grid[i].classList.add("grid");
+        }
+    }
+}
+
+function getSize(){
+    let input = prompt("Enter a size of the board, between 2 and 100:");
+    let massage = document.getElementById("message");
+    if(input !== null && !isNaN(input) && input >= 2 && input <= 100){
+        message.innerHTML = "Board size has been updated!"
+        return input;
+    }
+    else{
+        alert("Please enter a valid number!");
+    }
+}
+
+function colorDiv(){
+    if(color == "random"){
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+    }
+    else if (color == "black"){
+        this.style.backgroundColor = "black";
+    }
+    else if (color == "gray"){
+        this.style.backgroundColor = "gray";
+    }
+    else if (color == "white"){
+        this.style.backgroundColor = "white";
+    }
+
+}
+function setColor(colorChoice){
+    color = colorChoice;
+}
+function resetBoard(){
+    let divs = document.querySelectorAll(".grid");
+    divs.forEach((div) => div.style.backgroundColor = "white");
 }
